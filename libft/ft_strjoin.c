24ed10b796f9db8d4deff81adc7f46263fdb9970
Romain Lambert <romain@Romain.lan>
2024-03-10 18:45:03 +0100

    123

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: romlambe <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/04 11:29:39 by romlamb           #+#    #+#             */
+/*   Updated: 2024/03/10 18:28:37 by romlambe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*res;
+	size_t	len1;
+	size_t	len2;
+
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	if (!s1 && !s2)
+		return (0);
+	res = ft_calloc(len1 + len2 + 1, sizeof(char));
+	if (!res)
+		return (0);
+	ft_strlcpy(res, s1, len1 + 1);
+	ft_strlcat(res, s2, len1 + len2 + 1);
+	return (res);
+}
